8f292a51e3ffaace56c1f89dc416d048279e2530
Deviant <112615715+DEViantUA@users.noreply.github.com>
2022-11-12 18:50:26 +0300

    Update Example3.py

--- Example/Example3.py
+++ Example/Example3.py
@@ -7,6 +7,6 @@ the background to the image.
 
 from enkanetworkcard import encbanner
 
-client = encbanner.EnkaGenshinGeneration(img = ["8.png", "9.png"], adapt = True, randomImg = True) 
+ENC = encbanner.EnkaGenshinGeneration(img = ["8.png", "9.png"], adapt = True, randomImg = True) 
 
-b = client.start(uids = 724281429)
+result = ENC.start(uids = 724281429)
