bdc3e7ce509426040dd5740bd91587b600481f2a
Deviant <112615715+DEViantUA@users.noreply.github.com>
2022-11-12 18:38:18 +0300

    Update Example1.py

--- Example/Example1.py
+++ Example/Example1.py
@@ -1,1 +1,13 @@
-#Coming soon...
+#=====✦Description✦=====
+'''
+A simple example, with automatic saving of 
+finished results to the EnkaImg folder
+The folder will be created automatically
+'''
+
+from enkanetworkcard import encbanner
+
+client = encbanner.EnkaGenshinGeneration() 
+
+b = client.start(uids = 724281429)
+
