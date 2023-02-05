d20e60bd251f58e07f7bc9715da7faee584cba47
Deviant <112615715+DEViantUA@users.noreply.github.com>
2023-02-05 14:32:43 +0300

    Update openFile.py

--- enkanetworkcard/src/utils/openFile.py
+++ enkanetworkcard/src/utils/openFile.py
@@ -197,6 +197,6 @@ def __getattr__(name):
             try: 
                 cache[name] = image = Image.open(path)
             except UnidentifiedImageError:
-                cache[name] = image = dowload(path)
+                cache[name] = image = dowload(path,ret = False)
         
         return image
